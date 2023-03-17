d3aae36caeeff3e6404c07cdc95426e473563206
Gaspard Martin <gaspard.martin.jouannaud@gmail.com>
2023-03-17 14:13:46 +0100

    Ajoutez des fichiers projet.

---
+++ Lucas_Gaspard_projet_mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Lucas_Gaspard_projet_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
